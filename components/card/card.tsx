533e187680a3d21c1eb2999442c37be5d5ce470a
soufieddin <eddin2209@gmail.com>
2021-10-10 20:31:26 +0200

    update and fix

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { getHref } from '../../core/helpers';
 import { RecipeWithId } from "../../interfaces/recipe"
-import LinesEllipsis from 'react-lines-ellipsis';
 import styles from '../../styles/Card.module.css';
 
 type Props = {
@@ -15,13 +14,7 @@ const Card = ({ info }: Props) => {
         <img src={info.image} alt='recipe_image' className={styles.card__img} />
         <div className={styles.card__body}>
           <h3 className={styles.card__title}>{info.name}</h3>
-          <LinesEllipsis
-            text={info.shortDescription}
-            maxLine='1'
-            ellipsis='...'
-            trimRight
-            basedOn='letters'
-          />
+          <p>{info.shortDescription.slice(0, 48).concat('...')}</p>
         </div>
       </a>
     </Link>
