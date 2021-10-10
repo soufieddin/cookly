74feb0fd9b09cea8a316b726fc670a0aae32e6a3
soufieddin <eddin2209@gmail.com>
2021-10-10 21:53:29 +0200

    update and fix-3

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -14,7 +14,7 @@ const Card = ({ info }: Props) => {
         <img src={info.image} alt='recipe_image' className={styles.card__img} />
         <div className={styles.card__body}>
           <h3 className={styles.card__title}>{info.name}</h3>
-          <p>{info.shortDescription.slice(0, 48).concat('...')}</p>
+          <p>{info.shortDescription.slice(0, 45).concat('...')}</p>
         </div>
       </a>
     </Link>
